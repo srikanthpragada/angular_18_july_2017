fbd0cc8d02bf4ca343a5bd27c9fb0296d46c4b7e
srikanthpragada <srikanthpragada@gmail.com>
2017-07-27 08:10:30 +0530

    27-JULY

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,19 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
 import { ListBooksComponent} from "app/services/list-books.component";
 import { BookDetailsComponent} from "app/services/book-details.component";
 import { LoginComponent} from "app/forms/login.component";
+import { ReactiveLoginComponent} from "app/forms/reactive-login.component";
+
 
 @NgModule({
   declarations: [
-          ListBooksComponent,BookDetailsComponent, LoginComponent
+          ListBooksComponent,BookDetailsComponent,
+          LoginComponent, ReactiveLoginComponent
+          
   ],
   imports: [
-    BrowserModule, FormsModule
+    BrowserModule, FormsModule, ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [ LoginComponent ]
+  bootstrap: [ ReactiveLoginComponent ]
 })
 export class AppModule {
 
